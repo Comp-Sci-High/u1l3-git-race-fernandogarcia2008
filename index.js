71a621d0feb2e710bf72bd7ba407911abdc1ab42
fernandogarcia2008 <fernando.garciamoscoso26@compscihigh.org>
2024-09-18 15:19:25 +0000

    All the changes

--- index.js
+++ index.js
@@ -6,10 +6,12 @@
 // Task 2
 // Print your favorite snack
 "Goldfish"
+"chezz it"
 
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
 "Jcole"
+"Drake"
 // Stage and commit this change with an appropriate message
 // Go to more.js
--- more.js
+++ more.js
@@ -1,9 +1,11 @@
 // Task 4
 // Print your favorite food space
 "Pizzaera"
+"Stickys"
 // Stage and commit this change with an appropriate message
 // Task 5
 // Print your go to deli order
 "An albanian"
+"BEC"
 // Stage and commit this change with an appropriate message
 // Go to README.md
