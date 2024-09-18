d9bc6d453b331f9b3d50f923cc7c648a7716e646
fernandogarcia2008 <fernando.garciamoscoso26@compscihigh.org>
2024-09-18 15:12:34 +0000

    This is commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Task 1
 // Print your name
-fernando Garcia
+"fernando Garcia"
 
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
+"Goldfish"
 
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+"Jcole"
 // Stage and commit this change with an appropriate message
 // Go to more.js
